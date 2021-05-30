164adc813efcf6fe7c7f4bca5e09934beb81a8c3
jerry <jerry4905@hotmail.com>
2021-05-30 16:37:18 -0700

    city name and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 var recentSearchArr = [];
 
-//$(#recentSearches).Storage.getItem(recentCities);
 
 function getWeather() {
     var searchCity = $("#searchCity").val();
@@ -27,21 +26,39 @@ $("#recentSearches").on("click", ".btn", function (btn) {
     $("#searchCity").val(clickedRecentCity);
     getWeather();
 });
-var displayResults = function(cityName) {
+
+// display resuls on page
+function displayResults(cityName) {
     console.log(cityName)
+    var today = moment().format('MMMM Do YYYY')
+   
+    // update current day information form search 
+    $("#currentDate").text(today);
+    $("#currentTemp").text("Temp:");
+    $("#currentWindSpeed").text("Wind:");
+    $("#currentHumidity").text("Humidity:");
+    $("#currentUvIndex").text("UV:");
 };
 
+// save search results
 function saveRecent(cityName) {
+    //create button for cityName searched
     var recentSearchBtn = $("<input>", {
         type: "button",
         value: cityName,
         class: "btn btn-lg",
     });
+    displayResults();
+    //update city name 
+    $("#currentCity").text(cityName);
+   
     $("#recentSearches").append(recentSearchBtn);
+    // check to see if cityName duplcate
     var duplicate = verifyNoDup(cityName);
-    console.log("duplicates?" + duplicate);
+    console.log("duplicate check" + duplicate);
     if (!duplicate) {
         recentSearchArr.push(cityName)
+        // save to local storage 
         localStorage.setItem("recentCities", JSON.stringify(recentSearchArr));
         return;
     }
@@ -55,7 +72,7 @@ function saveRecent(cityName) {
             }
         }
     }
-    displayResults();
+   
 }
 
 
